63955b15080a0b8e23f80ad634c2c018917219c6
joonb14 <joonb14@gmail.com>
2021-11-01 21:35:18 +0900

    Refactoring done for first commit

---
+++ app/src/main/java/com/good/scanner/preference/PreferenceUtils.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2020 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.good.scanner.preference;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build.VERSION_CODES;
+import android.preference.PreferenceManager;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.annotation.StringRes;
+import androidx.camera.core.CameraSelector;
+import com.google.android.gms.common.images.Size;
+import com.google.common.base.Preconditions;
+import com.google.mlkit.common.model.LocalModel;
+import com.good.scanner.CameraSource;
+import com.good.scanner.CameraSource.SizePair;
+import com.google.mlkit.vision.demo.R;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+import com.google.mlkit.vision.objects.ObjectDetectorOptionsBase.DetectorMode;
+import com.google.mlkit.vision.objects.custom.CustomObjectDetectorOptions;
+import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions;
+import com.google.mlkit.vision.pose.PoseDetectorOptionsBase;
+import com.google.mlkit.vision.pose.accurate.AccuratePoseDetectorOptions;
+import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions;
+
+/** Utility class to retrieve shared preferences. */
+public class PreferenceUtils {
+
+  @Nullable
+  public static SizePair getCameraPreviewSizePair(Context context, int cameraId) {
+    Preconditions.checkArgument(
+        cameraId == CameraSource.CAMERA_FACING_BACK
+            || cameraId == CameraSource.CAMERA_FACING_FRONT);
+    String previewSizePrefKey;
+    String pictureSizePrefKey;
+    if (cameraId == CameraSource.CAMERA_FACING_BACK) {
+      previewSizePrefKey = context.getString(R.string.pref_key_rear_camera_preview_size);
+      pictureSizePrefKey = context.getString(R.string.pref_key_rear_camera_picture_size);
+    } else {
+      previewSizePrefKey = context.getString(R.string.pref_key_front_camera_preview_size);
+      pictureSizePrefKey = context.getString(R.string.pref_key_front_camera_picture_size);
+    }
+
+    try {
+      SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+      return new SizePair(
+          Size.parseSize(sharedPreferences.getString(previewSizePrefKey, null)),
+          Size.parseSize(sharedPreferences.getString(pictureSizePrefKey, null)));
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  @RequiresApi(VERSION_CODES.LOLLIPOP)
+  @Nullable
+  public static android.util.Size getCameraXTargetResolution(Context context, int lensfacing) {
+    Preconditions.checkArgument(
+        lensfacing == CameraSelector.LENS_FACING_BACK
+            || lensfacing == CameraSelector.LENS_FACING_FRONT);
+    String prefKey =
+        lensfacing == CameraSelector.LENS_FACING_BACK
+            ? context.getString(R.string.pref_key_camerax_rear_camera_target_resolution)
+            : context.getString(R.string.pref_key_camerax_front_camera_target_resolution);
+    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+    try {
+      return android.util.Size.parseSize(sharedPreferences.getString(prefKey, null));
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  public static boolean shouldHideDetectionInfo(Context context) {
+    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+    String prefKey = context.getString(R.string.pref_key_info_hide);
+    return sharedPreferences.getBoolean(prefKey, false);
+  }
+
+  public static boolean shouldGroupRecognizedTextInBlocks(Context context) {
+    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+    String prefKey = context.getString(R.string.pref_key_group_recognized_text_in_blocks);
+    return sharedPreferences.getBoolean(prefKey, true);
+  }
+
+  public static boolean isCameraLiveViewportEnabled(Context context) {
+    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+    String prefKey = context.getString(R.string.pref_key_camera_live_viewport);
+    return sharedPreferences.getBoolean(prefKey, false);
+  }
+
+  private PreferenceUtils() {}
+}
